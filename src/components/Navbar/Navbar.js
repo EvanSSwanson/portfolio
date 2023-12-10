8482ff061b14d1226594b5db2cf371e45dd9ec4f
Evan Swanson <evan.sundelius@gmail.com>
2023-12-09 22:20:52 -0600

    play with optional music idea

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,13 @@
 import './Navbar.css'
+import React, { useEffect, useState, useRef } from 'react'
 import logo from '../../resources/monitor-logo.png'
+import vid from "../../resources/wavy.mp4"
 
 const Navbar = () => {
+  const videoRef = useRef()
+  useEffect(() => {
+    videoRef.current.volume = .15
+  }, [])
 
   return (
     <div className='Navbar'>
@@ -16,9 +22,13 @@ const Navbar = () => {
             <a className= 'personal-link' id='personal-linkedin' target='_blank' rel='noopener noreferrer' href='https://www.linkedin.com/in/evan-swanson-5baa80158/'>LinkedIn</a>
             <a className= 'personal-link' id='personal-github' target='_blank' rel='noopener noreferrer' href='https://github.com/EvanSSwanson'>GitHub</a>
             <p id='personal-email'>evan.sundelius@gmail.com</p>
+            <p id='optional-vibe'>optional_vibe...</p>
+            <video id='video' ref={videoRef} loop playsinline controls>
+              <source src={vid} type="video/mp4" />
+            </video>
         </nav>
     </div>
-  );
+  )
 }
 
 export default Navbar
