1fd4747edc1c0f0de848903e9def3fd9aaaf9d6d
Evan Swanson <evan.sundelius@gmail.com>
2023-12-11 14:21:20 -0600

    alter about text

--- src/resources/aboutText.js
+++ src/resources/aboutText.js
@@ -1,5 +1,5 @@
 const aboutText1 = "Hi! Aren't you glad I'm not writing in"
 const aboutText2 = "this font?"
-const aboutText3 = "I'm Evan Sundelius Swanson, a frontend graduate from Turing who is super excited to start his career. I love designing apps, writing efficient code, and creating the best UX I can. With a background as a history major, my non-tech education has given me a unique perspective on programming. My real superpower lies in my passion for continuously learning; it's what attracted me to become a developer!"
+const aboutText3 = "I'm Evan Sundelius Swanson, a frontend graduate from Turing who is looking to make a difference for an organization that takes making a difference seriously. I love designing apps, writing efficient code, and creating the best UX I can. With a background as a history major, my non-tech education has given me a unique perspective on programming. My real superpower lies in my passion for continuously learning; it's what attracted me to become a developer!"
 
 export { aboutText1, aboutText2, aboutText3 }
